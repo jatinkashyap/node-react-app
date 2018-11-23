8ee9df099391d757881a3abbda81fba535f0c369
jatinkashyap <jatinkashyap9@gmail.com>
2018-11-22 21:46:21 -0500

    finish auth flow

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,27 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+// this function is to set cookie with (primary key)id of user
+// get mongoose model user and save its id in cookie
+passport.serializeUser((user,done) => {
+    console.log("Serializing");
+    done(null,user.id);
+});
+
+//now find the user in mongo DB by id from cookie we get
+passport.deserializeUser((id,done) => {
+    console.log("Deserializing");
+    User.findById(id)
+        .then(user => {
+            done(null,user);
+        });
+});
+
+// takesuser id, finds the user and set it as req.session
 
 passport.use(
     new GoogleStrategy(
@@ -9,8 +30,20 @@ passport.use(
         clientSecret : keys.googleClientSecret,
         callbackURL : '/auth/google/callback'
         }, 
-        (accessToken) => {
-            console.log(accessToken);
+        (accessToken,refreshToken,profile,done) => {
+            User.findOne({ googleId : profile.id})
+                .then((existingUser) => {
+                    if(existingUser){
+                        console.log("Already have the user");
+                        done(null,existingUser);
+                    }else{
+                        new User({ googleId : profile.id })
+                            .save()
+                            .then((user) => {
+                                done(null,user);
+                            });
+                    }
+                });
         }
     )
 );
