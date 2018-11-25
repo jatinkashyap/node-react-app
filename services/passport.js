f8438a6a5c9f61925bef046baac8053fab8902b9
jatinkashyap <jatinkashyap9@gmail.com>
2018-11-24 19:25:10 -0500

    refactored with async/await

--- services/passport.js
+++ services/passport.js
@@ -31,20 +31,16 @@ passport.use(
             callbackURL : '/auth/google/callback',
             proxy : true
         }, 
-        (accessToken,refreshToken,profile,done) => {
-            User.findOne({ googleId : profile.id})
-                .then((existingUser) => {
-                    if(existingUser){
-                        console.log("Already have the user");
-                        done(null,existingUser);
-                    }else{
-                        new User({ googleId : profile.id })
-                            .save()
-                            .then((user) => {
-                                done(null,user);
-                            });
-                    }
-                });
+        async (accessToken,refreshToken,profile,done) => {
+            const existingUser = await User.findOne({ googleId : profile.id});
+                if(existingUser){
+                    console.log("Already have the user");
+                    done(null,existingUser);
+                }else{
+                    console.log("Creating the new user");
+                    const user = await new User({ googleId : profile.id }).save();
+                    done(null,user);
+                }  
         }
     )
 );
